53c793d5d65d138a85301d535ea137ef3ecdbbdf
dharahash <87889389+dharahash@users.noreply.github.com>
2026-02-05 10:11:53 +0530

    Modify messages array in script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const messages = [
+    "Wrong answer!"
+    "Ahemmmmm!"
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "Dei, Really sure??",
+    "Chellakutty please...",
+    "Eyy please ra dei!, podii",
+    "I love youuu, now say yes!"
     "If you say no, I will be really sad...",
     "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "I will cryyyyy...",
+    "Ok fine po, I will go with someone else...",
+    "Just kidding, heheh say yes please mam ❤️"
 ];
 
 let messageIndex = 0;
